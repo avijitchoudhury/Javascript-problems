fc038c1486de0f9b24de0b36808e64567f89bfeb
Avijit Choudhury <avijit.choudhury24@gmail.com>
2019-12-17 11:22:46 -0500

    finding longest word in string sentence

--- js_problems.js
+++ js_problems.js
@@ -9,3 +9,18 @@ function factorialize(num) {
 }
 
 factorialize(5); //should return 120
+
+// Return the length of the longest word in the provided sentence.
+// Your response should be a number.
+
+function findLongestWordLength(str) {
+  let longestWord = 0;
+  let strSplitting = str.split(' ');
+  for (let element of strSplitting) {
+    if (element.length > longestWord) {
+      longestWord = element.length;
+    }
+  } return longestWord;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog"); //should return 6
