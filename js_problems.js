c78d0e8ed20d4eebdbeb50bff4cf2a03a6ddc8f0
Avijit Choudhury <avijit.choudhury24@gmail.com>
2019-12-17 13:05:37 -0500

    Repeat string

--- js_problems.js
+++ js_problems.js
@@ -50,3 +50,17 @@ function confirmEnding(str, target) {
 }
 
 confirmEnding("Bastian", "n");
+
+
+// Repeat a given string str(first argument) for num times(second argument).
+// Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+  let repeatedString = '';
+  while (num > 0) {
+    repeatedString += str;
+    num--;
+  } return repeatedString;
+}
+
+repeatStringNumTimes("abc", 3);
